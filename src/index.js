6b1e688c830f8f452b22bf2215ee75522fced427
cxh2763 <276314445@qq.com>
2022-01-28 15:09:25 +0800

    显示学生信息

--- src/index.js
+++ src/index.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import src1 from "./assets/1.jpg"
-import src2 from "./assets/2.jpg"
-import src3 from "./assets/3.jpg"
-import "./index.css"
+import StudentList from "./components/StudentList";
+const appkey = "276314445_1590670332060";
+/**
+ * 获取所有的学生数据
+ */
+ async function fetchAllStudents(){
+   const stus = await fetch("http://open.duyiedu.com/api/student/findAll?appkey="+appkey)
+        .then(resp => resp.json()).then(resp => resp.data)
+    return stus;
+ }
 
-const srcs = [src1,src2,src3];//保存图片路径的数组
+ async function render(){
+   ReactDOM.render("正在加载中....",document.getElementById("root"));
+   const stus = await fetchAllStudents() //获取学生数据
+   ReactDOM.render(<StudentList stus={stus}/>,document.getElementById("root"))
+ }
 
-let index = 0;//显示图片的索引
-
-const container = document.getElementById("root")
-
-let timer; //计时器
-
-function render(){
-    let img = (<img src = {srcs[index]} alt=""></img>)
-    ReactDOM.render(img,container);
-}
-render()
-// 启动计时器。每隔一段时间，切换图片
-function start(){
-    stop();
-    timer = setInterval(()=>{
-        index = (index + 1) % 3;//改变index
-        render();
-    },1500);
-}
-//停止计时器
-function stop(){
-    clearInterval(timer);
-}
-start()
-
-container.onmouseenter = function(){
-    stop()
-}
-container.onmouseleave =function(){
-    start()
-}
+ render();
