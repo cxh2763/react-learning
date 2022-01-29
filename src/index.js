f96fd6dc117d425c8ad0f80d72c34b5a6be256af
cxh2763 <276314445@qq.com>
2022-01-29 13:51:52 +0800

    自动移动的小球

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StudentList from "./components/StudentList";
-const appkey = "276314445_1590670332060";
-/**
- * 获取所有的学生数据
- */
- async function fetchAllStudents(){
-   const stus = await fetch("http://open.duyiedu.com/api/student/findAll?appkey="+appkey)
-        .then(resp => resp.json()).then(resp => resp.data)
-    return stus;
- }
+import React from "react";
+import ReactDom from "react-dom";
+import BallList from "./components/BallList"
 
- async function render(){
-   ReactDOM.render("正在加载中....",document.getElementById("root"));
-   const stus = await fetchAllStudents() //获取学生数据
-   ReactDOM.render(<StudentList stus={stus}/>,document.getElementById("root"))
- }
-
- render();
+ReactDom.render(<BallList/>,document.getElementById("root"))
