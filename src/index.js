88224d4bf22377f341317194a69e00425cb9f0aa
Cxh <276314445@qq.com>
2022-08-01 19:16:57 +0800

    feat :

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import src2 from "./assets/2.jpg"
 import src3 from "./assets/3.jpg"
 import "./index.css"
 
-const srcs = [src1,src2,src3];//保存图片路径的数组
+const srcs = [src1, src2, src3];//保存图片路径的数组
 
 let index = 0;//显示图片的索引
 
@@ -13,28 +13,28 @@ const container = document.getElementById("root")
 
 let timer; //计时器
 
-function render(){
-    let img = (<img src = {srcs[index]} alt=""></img>)
-    ReactDOM.render(img,container);
+function render() {
+    let img = (<img src={srcs[index]} alt=""></img>)
+    ReactDOM.render(img, container);
 }
 render()
 // 启动计时器。每隔一段时间，切换图片
-function start(){
+function start() {
     stop();
-    timer = setInterval(()=>{
+    timer = setInterval(() => {
         index = (index + 1) % 3;//改变index
         render();
-    },1500);
+    }, 1500);
 }
 //停止计时器
-function stop(){
+function stop() {
     clearInterval(timer);
 }
 start()
 
-container.onmouseenter = function(){
+container.onmouseenter = function () {
     stop()
 }
-container.onmouseleave =function(){
+container.onmouseleave = function () {
     start()
 }
